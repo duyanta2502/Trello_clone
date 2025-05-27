d45de8cccaf1a90719185621123f32cb72012132
An <tda25022003@gmail.com>
2025-05-27 15:58:52 +0700

    dark_light

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,16 +4,34 @@ import AccessAlarm from '@mui/icons-material/AccessAlarm'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>taduyan</div>
+
       <Typography variant="body2" color="text.secondary">Test typo</Typography>
+      
       <Button variant="text">Text</Button>
       <Button variant="contained" color="success">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-      <br></br>
+      <br />
       <AccessAlarm />
       <ThreeDRotation />
       <HomeIcon />
