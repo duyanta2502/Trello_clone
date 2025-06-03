ce3eabf17007849009be1b77d487e4794263fab2
An <tda25022003@gmail.com>
2025-06-03 11:15:23 +0700

    create_appbar

---
+++ Frontend/src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+
+
+function Profiles() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          aria-controls={open ? 'account-menu-profile' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          sx={{ padding: 0, width: 40, height: 40 }}
+        >
+          <Avatar
+            sx={{ width: 32, height: 32 }}
+            alt="Profile"
+            src='https://cdn.donmai.us/720x720/c4/ff/c4ffab6a2ac05f28c76333f35f56ceae.webp'
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-Profiles"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profiles'
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: 30, height: 30, mr: 1 }} src='https://cdn.donmai.us/720x720/c4/ff/c4ffab6a2ac05f28c76333f35f56ceae.webp' /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: 30, height: 30, mr: 1 }} src='https://cdn.donmai.us/720x720/c4/ff/c4ffab6a2ac05f28c76333f35f56ceae.webp' /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
--- Frontend/src/pages/Boards/_id.jsx
+++ Frontend/src/pages/Boards/_id.jsx
@@ -5,7 +5,8 @@ import BoardContent from './BoardContent'
 
 function Board() {
   return (
-    <Container disableGutters maxWidth = {false} sx ={{ height: '100vh', backgroundColor: 'primary.main' }}>
+    // , backgroundColor: 'primary.main'
+    <Container disableGutters maxWidth = {false} sx ={{ height: '100vh' }}>
       <AppBar />
       <BoardBar />
       <BoardContent />
