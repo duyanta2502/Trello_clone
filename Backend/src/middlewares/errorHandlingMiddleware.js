04d0435ce975f2ae7f4349daa021b095baa461e2
An <tda25022003@gmail.com>
2025-06-19 08:35:24 +0700

    add prd_dev Mode

--- Backend/src/middlewares/errorHandlingMiddleware.js
+++ Backend/src/middlewares/errorHandlingMiddleware.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { StatusCodes } from 'http-status-codes'
+import { env } from '~/config/environment.js'
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   if ( !err.statusCode ) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
@@ -9,6 +10,8 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     message: err.message || StatusCodes[err.statusCode],
     stack: err.stack
   }
-
+  // production mode không trả về stack trace
+  console.log(env.BUILD_MODE)
+  if (env.BUILD_MODE !== 'dev') delete responseError.stack
   res.status(responseError.statusCode).json(responseError)
 }
